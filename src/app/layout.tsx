0b43f967d263b00d456733e6a525c78d4d288262
Enis Zekiqi <99254753+EnisZekiqi@users.noreply.github.com>
2025-05-20 00:20:41 +0200

    added drawer for responsive & folder structure is fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,9 @@ import "./globals.css";
 import { Raleway } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from "./lib/queryClient";
+import Providers from "./lib/Providers";
 const raleway = Raleway({ subsets: ["latin"], variable: "--font-raleway" });
 
 export const metadata: Metadata = {
@@ -20,9 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${raleway.className} antialiased`}>
+      <Providers>
         <Navbar/>
         {children}
-        <Footer/>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
