d3e93262e30666526da55a4dc378202dc637af57
Enis Zekiqi <99254753+EnisZekiqi@users.noreply.github.com>
2025-06-08 20:12:27 +0200

    changes on the resource detail to remove the bug on the deployment

--- src/app/resources/[id]/page.tsx
+++ src/app/resources/[id]/page.tsx
@@ -11,150 +11,131 @@ type PageProps = {
   searchParams?: { type?: string };
 };
 
-export default async function ResourceDetail({ params, searchParams }: PageProps) {
-  const { id } = params;
-  const numericId = Number(id);
-  if (isNaN(numericId)) return notFound();
+const ResourceDetail = async ({ params, searchParams }: PageProps): Promise<React.ReactElement | null> => {
+  {
+    const { id } = params;
+    const numericId = Number(id);
+    if (isNaN(numericId)) return notFound();
 
-  const { type } = searchParams ?? {};
-  if (!type || !["article", "repo", "tools"].includes(type)) {
+    const { type } = searchParams ?? {};
+    if (!type || !["article", "repo", "tools"].includes(type)) {
+      return notFound();
+    }
+
+    if (type === "article") {
+      const article = await fetchArticleById(numericId);
+      if (!article) return notFound();
+
+      return (
+        <div className="p-10 h-full w-full mt-[15%] sm:mt-[5%] -ml-2 sm:ml-[25%]">
+          <div className="flex items-center gap-4 mb-4">
+            <img src={article.user.profile_image} alt={article.user.name} className="w-10 h-10 rounded-full" />
+            <p className="text-sm">{article.user.name}</p>
+          </div>
+          <h1 className="text-xl sm:text-2xl font-bold">{article.title}</h1>
+          <p className="mt-4 text-sm text-gray-400 w-full sm:w-2/3">{article.description}</p>
+
+          <div className="flex gap-3 mt-2">
+            {typeof article.tag_list === 'string' && article.tag_list ? (
+              (article.tag_list as string).split(',').map((tag: string) => (
+                <span key={tag} className="bg-gray-800 text-white text-xs px-2 py-1 rounded">
+                  {tag.trim()}
+                </span>
+              ))
+            ) : null}
+          </div>
+
+          {article.cover_image ? (
+            <img src={article.cover_image} alt="Cover" className="mt-4 object-contain w-[700px]" />
+          ) : (
+            <div className="text-sm text-gray-400 mt-4">No cover image available.</div>
+          )}
+          <span className="mt-4 mb-4 flex items-center text-lg"><IoMdHeart size={22} />{article.public_reactions_count}</span>
+          <a href={article.url} className="mt-8 text-blue-400 underline w-fit">Visit full article</a>
+        </div>
+      );
+    }
+
+    if (type === "repo") {
+      const repo = await fetchRepoById(numericId);
+      if (!repo) return notFound();
+
+      return (
+        <div className="h-full w-full flex flex-col mt-[25%] sm:mt-[8%] ml-7 sm:ml-[25%]">
+          <div className="flex justify-between items-center w-[90%] sm:w-[50%]">
+            <div className="flex items-center gap-3">
+              <img className="w-10 h-10 border border-[#343434] rounded-full" src={repo.owner.avatar_url} alt="" />
+              <h1 className="text-2xl font-bold">{repo.name}</h1>
+            </div>
+            <SaveRepoButton repo={repo} />
+          </div>
+          <p className="mt-4 text-sm text-gray-300 w-full sm:w-[55%]">{repo.description}</p>
+          <div className="flex flex-wrap w-[90%] sm:w-2/4 items-center gap-1.5 mt-4">
+            {repo.topics?.map((tag: string) => (
+              <span key={tag} className="bg-gray-800 text-white text-xs px-2 py-1 rounded">{tag}</span>
+            ))}
+          </div>
+          <p className="mt-4">Language: <span style={{ color: repo.language === 'JavaScript' ? 'yellow' : (repo.language === 'TypeScript' ? 'aqua' : (repo.language === 'C++' ? 'blue' : '#343434')) }}>{repo.language}</span></p>
+          <p className="mt-2 flex items-center gap-1"><IoMdStarOutline size={23} />: <span className="text-gray-400">{repo.stargazers_count} stars</span></p>
+          <p className="mt-2 flex items-center gap-1"><AiOutlineFork size={22} />: <span className="text-gray-400">{repo.forks_count} forks</span></p>
+          <p className="mt-2 flex items-center gap-1"><IoMdCalendar size={23} />: <span className="text-gray-400">{repo.updated_at}</span></p>
+          <p className="mt-2 flex items-center gap-1">License: <span className="text-gray-400">{repo.license?.name}</span></p>
+          <p className="mt-2 flex items-center gap-1">Owner Repository: <span className="text-gray-400">{repo.owner.login}</span></p>
+          <a href={repo.html_url} target="_blank" className="text-[#0251EF] underline mt-4 block w-fit">View on GitHub</a>
+        </div>
+      );
+    }
+
+    if (type === "tools") {
+      const tool = tools.find((tool) => Number(tool.id) === numericId);
+      if (!tool) return notFound();
+
+      return (
+        <div className="h-full w-full flex flex-col mt-[28%] sm:mt-[8%] ml-7 sm:ml-[25%] max-w-[700px]">
+          <div className="flex items-center gap-3 mb-4">
+            <img src={tool.icon} alt={tool.name} className="w-10 h-10" />
+            <h1 className="text-2xl font-bold">{tool.name}</h1>
+          </div>
+
+          <p className="text-sm text-gray-300">{tool.description}</p>
+
+          <div className="flex flex-wrap gap-2 mt-3">
+            {tool.tags?.map((tag: string) => (
+              <span key={tag} className="bg-gray-800 text-white text-xs px-2 py-1 rounded">{tag.trim()}</span>
+            ))}
+          </div>
+
+          <div className="mt-4">
+            <h2 className="font-semibold">Category:</h2>
+            <p className="text-sm text-gray-400">{tool.category}</p>
+          </div>
+
+          <div className="mt-2">
+            <h2 className="font-semibold">Features:</h2>
+            <p className="text-sm text-gray-400">{tool.features}</p>
+          </div>
+
+          <div className="mt-2">
+            <h2 className="font-semibold">Pricing:</h2>
+            <p className="text-sm text-gray-400">{tool.pricing}</p>
+          </div>
+
+          <div className="mt-2">
+            <h2 className="font-semibold">Integration Support:</h2>
+            <p className="text-sm text-gray-400">{tool.integrationSupport}</p>
+          </div>
+
+          <a href={tool.url} target="_blank" className="text-blue-400 underline mt-4 block w-fit">
+            Visit {tool.name}
+          </a>
+        </div>
+      );
+    }
+
     return notFound();
   }
 
-  if (type === "article") {
-    const article = await fetchArticleById(numericId);
-    if (!article) return notFound();
 
-    return (
-      <div className="p-10 h-full w-full mt-[15%] sm:mt-[5%] -ml-2 sm:ml-[25%]">
-        <div className="flex items-center gap-4 mb-4">
-          <img src={article.user.profile_image} alt={article.user.name} className="w-10 h-10 rounded-full" />
-          <p className="text-sm">{article.user.name}</p>
-        </div>
-        <h1 className="text-xl sm:text-2xl font-bold">{article.title}</h1>
-        <p className="mt-4 text-sm text-gray-400 w-full sm:w-2/3">{article.description}</p>
-
-        <div className="flex gap-3 mt-2">
-          {typeof article.tag_list === 'string' && article.tag_list ? (
-            (article.tag_list as string).split(',').map((tag: string) => (
-              <span key={tag} className="bg-gray-800 text-white text-xs px-2 py-1 rounded">
-                {tag.trim()}
-              </span>
-            ))
-          ) : null}
-        </div>
-
-        {article.cover_image ? (
-          <img src={article.cover_image} alt="Cover" className="mt-4 object-contain w-[700px]" />
-        ) : (
-          <div className="text-sm text-gray-400 mt-4">No cover image available.</div>
-        )}
-        <span className="mt-4 mb-4 flex items-center text-lg"><IoMdHeart size={22} />{article.public_reactions_count}</span>
-        <a href={article.url} className="mt-8 text-blue-400 underline w-fit">Visit full article</a>
-      </div>
-    );
-  }
-
-  if (type === "repo") {
-    const repo = await fetchRepoById(numericId);
-    if (!repo) return notFound();
-
-    return (
-      <div className="h-full w-full flex flex-col mt-[25%] sm:mt-[8%] ml-7 sm:ml-[25%]">
-        <div className="flex justify-between items-center w-[90%] sm:w-[50%]">
-          <div className="flex items-center gap-3">
-            <img className="w-10 h-10 border border-[#343434] rounded-full" src={repo.owner.avatar_url} alt="" />
-            <h1 className="text-2xl font-bold">{repo.name}</h1>
-          </div>
-          <SaveRepoButton repo={repo} />
-        </div>
-        <p className="mt-4 text-sm text-gray-300 w-full sm:w-[55%]">{repo.description}</p>
-        <div className="flex flex-wrap w-[90%] sm:w-2/4 items-center gap-1.5 mt-4">
-          {repo.topics?.map((tag: string) => (
-            <span key={tag} className="bg-gray-800 text-white text-xs px-2 py-1 rounded">{tag}</span>
-          ))}
-        </div>
-        <p className="mt-4">Language: <span style={{ color: repo.language === 'JavaScript' ? 'yellow' : (repo.language === 'TypeScript' ? 'aqua' : (repo.language === 'C++' ? 'blue' : '#343434')) }}>{repo.language}</span></p>
-        <p className="mt-2 flex items-center gap-1"><IoMdStarOutline size={23} />: <span className="text-gray-400">{repo.stargazers_count} stars</span></p>
-        <p className="mt-2 flex items-center gap-1"><AiOutlineFork size={22} />: <span className="text-gray-400">{repo.forks_count} forks</span></p>
-        <p className="mt-2 flex items-center gap-1"><IoMdCalendar size={23} />: <span className="text-gray-400">{repo.updated_at}</span></p>
-        <p className="mt-2 flex items-center gap-1">License: <span className="text-gray-400">{repo.license?.name}</span></p>
-        <p className="mt-2 flex items-center gap-1">Owner Repository: <span className="text-gray-400">{repo.owner.login}</span></p>
-        <a href={repo.html_url} target="_blank" className="text-[#0251EF] underline mt-4 block w-fit">View on GitHub</a>
-      </div>
-    );
-  }
-
-  if (type === "tools") {
-    const tool = tools.find((tool) => Number(tool.id) === numericId);
-    if (!tool) return notFound();
-
-    return (
-      <div className="h-full w-full flex flex-col mt-[28%] sm:mt-[8%] ml-7 sm:ml-[25%] max-w-[700px]">
-        <div className="flex items-center gap-3 mb-4">
-          <img src={tool.icon} alt={tool.name} className="w-10 h-10" />
-          <h1 className="text-2xl font-bold">{tool.name}</h1>
-        </div>
-
-        <p className="text-sm text-gray-300">{tool.description}</p>
-
-        <div className="flex flex-wrap gap-2 mt-3">
-          {tool.tags?.map((tag: string) => (
-            <span key={tag} className="bg-gray-800 text-white text-xs px-2 py-1 rounded">{tag.trim()}</span>
-          ))}
-        </div>
-
-        <div className="mt-4">
-          <h2 className="font-semibold">Category:</h2>
-          <p className="text-sm text-gray-400">{tool.category}</p>
-        </div>
-
-        <div className="mt-2">
-          <h2 className="font-semibold">Features:</h2>
-          <p className="text-sm text-gray-400">{tool.features}</p>
-        </div>
-
-        <div className="mt-2">
-          <h2 className="font-semibold">Pricing:</h2>
-          <p className="text-sm text-gray-400">{tool.pricing}</p>
-        </div>
-
-        <div className="mt-2">
-          <h2 className="font-semibold">Integration Support:</h2>
-          <p className="text-sm text-gray-400">{tool.integrationSupport}</p>
-        </div>
-
-        <a href={tool.url} target="_blank" className="text-blue-400 underline mt-4 block w-fit">
-          Visit {tool.name}
-        </a>
-      </div>
-    );
-  }
-
-  return notFound();
 }
+export default ResourceDetail;
 
-// ✅ generateStaticParams for SSG export
-export async function generateStaticParams() {
-  const [articles, repos] = await Promise.all([
-    fetchArticles(),
-    fetchRepositories()
-  ]);
-
-  const articleParams = articles.map((article: any) => ({
-    id: article.id.toString(),
-    type: "article"
-  }));
-
-  const repoParams = repos.map((repo: any) => ({
-    id: repo.id.toString(),
-    type: "repo"
-  }));
-
-  const toolParams = tools.map((tool: any) => ({
-    id: tool.id.toString(),
-    type: "tools"
-  }));
-
-  return [...articleParams, ...repoParams, ...toolParams];
-}
